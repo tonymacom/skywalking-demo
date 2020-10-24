c69257015b8fa600d917cd89d16cb5085ce17b19
itmabo <itmabo@163.com>
2020-10-24 17:08:49 +0800

    cc

--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 mvn clean install -DskipTests
 
-docker build -t itmabo/skywalking-agent:latest . -f Dockerfile-Agent
-docker push itmabo/skywalking-agent:latest
-
 docker build -t itmabo/sky:v1.0.0 . -f Dockerfile-Sky
 docker push itmabo/sky:v1.0.0
 
 docker build -t itmabo/bees:v1.0.0 . -f Dockerfile-Bees
-docker push bees:v1.0.0
+docker push itmabo/bees:v1.0.0
 
 kubectl delete -f Deployment.yaml
 kubectl apply -f Deployment.yaml
