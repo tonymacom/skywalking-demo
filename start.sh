1638b73165f818eb4829ee28eb241956e114a3cc
itmabo <itmabo@163.com>
2020-10-24 11:43:56 +0800

    fix dockerfile

--- start.sh
+++ start.sh
@@ -10,4 +10,5 @@ docker push itmabo/sky:v1.0.0
 docker build -t itmabo/bees:v1.0.0 . -f Dockerfile-Bees
 docker push bees:v1.0.0
 
+kubectl delete -f Deployment.yaml
 kubectl apply -f Deployment.yaml
