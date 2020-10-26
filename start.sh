8e3a4ecb1f20006fe7846e9636b4458a7867590e
itmabo <itmabo@163.com>
2020-10-26 18:00:23 +0800

    fix bat

--- start.sh
+++ start.sh
@@ -7,5 +7,4 @@ docker push itmabo/sky:v1.0.0
 docker build -t itmabo/bees:v1.0.0 . -f Dockerfile-Bees
 docker push itmabo/bees:v1.0.0
 
-kubectl delete -f Deployment.yaml
-kubectl apply -f Deployment.yaml
+kubectl apply -f ui-deployment.yaml -f skywalking-agent.yaml -f oap-deploymnet.yaml -f sky-deployment.yaml -f bees-deployment.yaml
